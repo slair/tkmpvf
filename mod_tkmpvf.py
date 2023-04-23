fa8cd4135999772744daef4242f2da002ce0aa29
slair u1e <slair@users.noreply.github.com>
2023-04-23 20:23:47 +0400

    autocommit

--- mod_tkmpvf.py
+++ mod_tkmpvf.py
@@ -531,7 +531,7 @@ class Application(tk.Frame):
 		self.lf = tk.Frame(self)
 		self.lf.pack(side="top", fill="both", expand=True)
 
-		self.tFont = ("Liberation Serif", 24)
+		self.tFont = ("Liberation Sans Narrow", 24)
 
 		self.df = tk.Frame(self.lf, bg="#800000")
 		self.df.pack(side="left", fill=None, expand=False)
